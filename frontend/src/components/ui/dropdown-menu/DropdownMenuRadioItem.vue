6bf524bba58080cbf89350ee64b79a48c5f2553b
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-12-17 09:09:42 +0000

    remove nixpacks

--- frontend/src/components/ui/dropdown-menu/DropdownMenuRadioItem.vue
+++ frontend/src/components/ui/dropdown-menu/DropdownMenuRadioItem.vue
@@ -37,7 +37,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     <span class="absolute left-2 flex size-3.5 items-center justify-center">
       <DropdownMenuItemIndicator>
         <!-- eslint-disable-next-line tailwindcss/no-custom-classname -->
-        <DotFilledIcon class="fill-current size-4" />
+        <DotFilledIcon class="size-4 fill-current" />
       </DropdownMenuItemIndicator>
     </span>
     <slot />
